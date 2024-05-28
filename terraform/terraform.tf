547ff3829ac833f943ed90e32cc9dd165e90361d
sgapea <sgapea@yahoo.com>
2024-05-28 12:27:20 -0500

    initial commit

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "cloudcore007"
+    bucket = "slyappbucket"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 
   required_version = ">= 1.6.3"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,7 +44,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c7217cdde317cfec" // Replace with the latest AMI ID for your region
+  default     = "ami-09040d770ffe2224f" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
